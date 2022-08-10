7f0f170e4cc2b183da346cca38678107bd41bf1c
MNXACb <mihailbykov@mail.ru>
2022-08-10 09:37:04 +0800

    add list Rases

--- Webinar_2/Program.cs
+++ Webinar_2/Program.cs
@@ -1,10 +1,22 @@
-﻿class Program
+﻿/*
+int[] numbers = { 1, 3, 4, 546, 67, 4 }; // массив указывается квадратными скобками
+System.Console.WriteLine(numbers[0]); // 0 - 1, 1 - 3, 2 - 4 ...
+int len = numbers.Length;
+Console.WriteLine(numbers[numbers.Length - 1]);
+*/
+
+class Program
 {
-    static void Main(string[] agrs) 
+    static void SelectRase()
     {
-        int[] numbers = { 1, 3, 4, 546, 67, 4 }; // массив указывается квадратными скобками
-        System.Console.WriteLine(numbers[0]); // 1
-        int len = numbers.Length;
-        Console.WriteLine(numbers[numbers.Length - 1]);
+        string[] namesRase = { "Люди", "Эльфы", "Орки", "гоблины", "Гномы" };
+        for (int i = 0; i < namesRase.Length-1; i++)
+        {
+            Console.Write(i + 1 + " - " + namesRase[i] + ", ");
+        }
+    }
+    static void Main(string[] agrs)
+    {
+        SelectRase();
     }
 }
