b3bc0114461a8bd81c80db4f8bb6bc2816266efb
MNXACb <mihailbykov@mail.ru>
2022-08-10 09:02:47 +0800

    start webinar

---
+++ Webinar_2/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] agrs) 
+    {
+        int[] numbers = { 1, 3, 4, 546, 67, 4 }; // массив указывается квадратными скобками
+        System.Console.WriteLine(numbers[0]); // 1
+        int len = numbers.Length;
+        Console.WriteLine(numbers[numbers.Length - 1]);
+    }
+}
